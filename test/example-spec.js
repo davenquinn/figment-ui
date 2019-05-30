2e961e860e1ddbb4bbb394066d29a8e19dbc79fa
Daven Quinn <dev@davenquinn.com>
2019-05-30 06:51:58 -0500

    Update testing data

--- test/example-spec.js
+++ test/example-spec.js
@@ -1,4 +1,4 @@
-printer = new Printer buildDir: "#{__dirname}/output"
+const printer = new Printer(buildDir: `${__dirname}/output`);
 printer.task('simple-svg.pdf', './simple-svg')
 printer.task('large-svg.pdf', './large-svg')
 module.exports = printer
