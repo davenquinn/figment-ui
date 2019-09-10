aff984892049b123218146aa5ff84d57edc729b3
Daven Quinn <dev@davenquinn.com>
2019-09-10 07:18:36 -0400

    Added syntax error spec

--- test/example-spec.js
+++ test/example-spec.js
@@ -2,4 +2,5 @@
 const printer = new Printer({buildDir: __dirname+"/output"});
 printer.task('simple-svg.pdf', './simple-svg.coffee');
 printer.task('large-svg.pdf', './large-svg.coffee');
+printer.task('syntax-error.pdf', './syntax-error.coffee');
 module.exports = printer
---
+++ test/syntax-error.coffee
@@ -0,0 +1,10 @@
+{select} = require 'd3-selection'
+
+module.exports = (el_, callback)->
+  # Create our svg
+  el = select el_
+    .append 'svg'asdas
+    .attr 'width', 200
+    .attr 'height', 100
+
+  callback()
