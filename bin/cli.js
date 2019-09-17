c0fa9daeabf3a4d276fb69b4a913ece0c6de0101
Daven Quinn <dev@davenquinn.com>
2019-09-17 02:38:21 -0500

    Updated application bootstrap script

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,10 @@
-#!/usr/bin/env npx run-electron
-require("../main/cli.js")
+#!/usr/bin/env node
+'use strict';
+
+// This code was liberated from sindresorhus/run-electron
+const {spawn} = require('child_process');
+const electronRunner = require.resolve('run-electron/cli.js');
+
+const mainFile = require.resolve("../main/cli.js");
+const args = [mainFile, ...process.argv.slice(2)];
+const cp = spawn(electronRunner, args, {stdio: 'inherit'});
