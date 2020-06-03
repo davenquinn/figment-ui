1df3d114ec5ffb3b6f1ff4da86ffb5785e2466f6
Daven Quinn <dev@davenquinn.com>
2020-06-03 04:06:21 -0500

    Add a test of png compilation

--- test/example-spec.js
+++ test/example-spec.js
@@ -3,6 +3,7 @@ const Figment = require('../lib').default;
 let v = new Figment({buildDir: __dirname+"/output"});
 v.task('simple-svg.pdf', './simple-svg.coffee');
 v.task('large-svg.pdf', './large-svg.coffee');
+v.task('large-svg.png', './large-svg.coffee');
 v.task('webpack.pdf', './large-svg.coffee', {webpackConfig: "./webpack.config.js"})
 v.task('syntax-error.pdf', './syntax-error.coffee');
 v.task('react-component.pdf', './react-component.coffee');
